54c2dc943309a4fefc757f545fe934c269224389
sunjid-git <sunjid.info@gmail.com>
2021-09-20 22:11:25 +0600

    first commit

---
+++ src/component/About/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./About.css";
+
+import img1 from '../../images/offset_comp_772626-opt 1.png';
+import img2 from '../../images/offset_comp_772626-opt 2.png';
+import img3 from '../../images/offset_comp_772626-opt 3.png';
+
+export default function About() {
+  return (
+    <>
+      <section className="about">
+        <div className="about-left">
+            <img src={img1} alt="" />
+            <img src={img2} alt="" />
+            <img src={img3} alt="" />
+        </div>
+        <div className="about-right">
+          <h1>About Us</h1>
+          <p>
+            Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder
+            text used in design when creating content. It helps designers plan
+            out where the content will sit
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/component/Home/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Home.css";
+
+import arrow1 from "../../images/Arrow 1.png";
+import arrow2 from "../../images/Arrow 2.png";
+
+export default function Home() {
+  return (
+    <>
+      <section className="home">
+        <div className="home-inner-menu">
+          
+          <div className="menu-items">
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Services & Package</li>
+              <li>Gallery</li>
+              <li>Package</li>
+              <li>FAQ</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+
+          <div className="home-inner">
+            <img src={arrow2} alt="" />
+            <div className="home-text">
+              <h1>A picture is worth a thousand words</h1>
+              <p>
+                It clearly and beautifully captures your emotions- the joy, the
+                laughter, the tears, you name it. Our pride is in offering the
+                best shoots as we help you tell your story in photos.
+              </p>
+              <p className="view-gallery">View Gallery</p>
+            </div>
+            <img src={arrow1} alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
